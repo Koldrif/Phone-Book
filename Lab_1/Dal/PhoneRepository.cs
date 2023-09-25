becc8a0c824db851664cabe325f1c116819e6f08
Igor <vorobiov.777@yandex.ru>
2023-09-25 21:24:55 +0300

    Added phone repository

---
+++ Lab_1/Dal/PhoneRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Lab_1.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Lab_1.Dal
+{
+	public class PhoneRepository
+    {
+        private string dataFilePath;
+        private readonly CsvConfiguration _csvConfiguration;
+
+        public PhoneRepository(IConfiguration configuration)
+        {
+            dataFilePath = configuration.GetSection("LastFilePath").Value;
+            _csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
+        }
+
+        public List<PhoneModel> GetAll()
+        {
+
+            using StreamReader reader = new StreamReader(dataFilePath);
+            using CsvReader csvReader = new CsvReader(reader, _csvConfiguration);
+            csvReader.Read();
+            return csvReader.GetRecords<PhoneModel>().ToList();
+        }
+    }
+}
