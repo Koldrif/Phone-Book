91516296cd9608a8d6c1f9a5cc3158089992fe9b
Igor <vorobiov.777@yandex.ru>
2023-09-25 21:25:35 +0300

    Added IoC, update settings method

--- Lab_1/MainWindow.xaml.cs
+++ Lab_1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Lab_1.Dal;
 using Lab_1.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Path = System.IO.Path;
 
 namespace Lab_1
 {
@@ -23,24 +28,36 @@ namespace Lab_1
 	public partial class MainWindow : Window
 	{
 		private readonly IConfiguration _configuration;
+		private readonly IServiceProvider _serviceProvider;
 		public MainWindow()
 		{
 			InitializeComponent();
-			_configuration = new ConfigurationManager().AddJsonFile("")
+			_configuration = new ConfigurationManager()
+				.AddJsonFile("appsettings.json", false, true)
+				.Build();
+
+			IServiceCollection services = new ServiceCollection();
+
+			services.AddSingleton(_configuration);
+			services.AddSingleton<PhoneRepository>();
+
+			_serviceProvider = services.BuildServiceProvider();
 		}
 
 
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
-			var tmp = new List<PhoneModel>()
+			var tmpPath = _configuration.GetSection("LastFilePath").Value;
+			if (!File.Exists(tmpPath))
 			{
-				new PhoneModel()
-				{
-					PersonName = "Igor Vorobyev",
-					PhoneNumber = "+79057461868"
-				}
-			};
-			DataTable.ItemsSource = tmp;
+				var pathToFile = Path.Combine(Directory.GetCurrentDirectory(), "phoneData.csv");
+				File.Create(pathToFile).Close();
+				_configuration.GetRequiredSection("LastFilePath").Value = pathToFile;
+				var tmpParams = _configuration.GetSection("LastFilePath");
+				File.WriteAllText("appsettings.json", JsonConvert.SerializeObject(new { LastFilePath = tmpParams.Value}, Formatting.Indented));
+			}
+
+			DataTable.ItemsSource = _serviceProvider.GetRequiredService<PhoneRepository>().GetAll();
 		}
 
 		private void ChangeButton_OnClick(object sender, RoutedEventArgs e)
