88fe1d31eee35ec70fb1865a9bb14ad2d1800a5e
Igor <vorobiov.777@yandex.ru>
2023-09-26 14:36:03 +0300

    Added AppSetting instead of configuration

---
+++ Lab_1/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+	public class AppSettings
+	{
+		public string LastFilePath { get; set; }
+
+
+		public void UpdateConfigurationFile()
+		{
+			File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"), JsonConvert.SerializeObject(this));
+		}
+	}
+}
--- Lab_1/MainWindow.xaml.cs
+++ Lab_1/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using Lab_1.Dal;
 using Lab_1.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 using Path = System.IO.Path;
 
@@ -27,42 +28,61 @@ namespace Lab_1
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private readonly IConfiguration _configuration;
+		//private readonly IConfiguration _configuration;
 		private readonly IServiceProvider _serviceProvider;
+		private PhoneRepository _phoneRepository;
+		private readonly AppSettings _appSettings;
 		public MainWindow()
 		{
 			InitializeComponent();
-			_configuration = new ConfigurationManager()
-				.AddJsonFile("appsettings.json", false, true)
-				.Build();
+			//_configuration = new ConfigurationManager()
+			//	.AddJsonFile("appsettings.json", false, true)
+			//	.Build();
 
 			IServiceCollection services = new ServiceCollection();
 
-			services.AddSingleton(_configuration);
+			var currentDirectory = Directory.GetCurrentDirectory();
+			var filePath = Path.Combine(currentDirectory, "appsettings.json");
+			_appSettings = JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText(filePath));
+			if (_appSettings == null)
+			{
+				throw new FileLoadException("Unable to read appsetting.json");
+			}
+			services.AddSingleton(_appSettings);
+
 			services.AddSingleton<PhoneRepository>();
+			services.AddTransient<PhoneEntry>();
+
 
 			_serviceProvider = services.BuildServiceProvider();
+
 		}
 
 
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
-			var tmpPath = _configuration.GetSection("LastFilePath").Value;
-			if (!File.Exists(tmpPath))
+			if (string.IsNullOrEmpty(_appSettings.LastFilePath))
 			{
-				var pathToFile = Path.Combine(Directory.GetCurrentDirectory(), "phoneData.csv");
-				File.Create(pathToFile).Close();
-				_configuration.GetRequiredSection("LastFilePath").Value = pathToFile;
-				var tmpParams = _configuration.GetSection("LastFilePath");
-				File.WriteAllText("appsettings.json", JsonConvert.SerializeObject(new { LastFilePath = tmpParams.Value}, Formatting.Indented));
+				_appSettings.LastFilePath = Path.Combine(Directory.GetCurrentDirectory(), "phoneData.csv");
+				_appSettings.UpdateConfigurationFile();
 			}
+			if (!File.Exists(_appSettings.LastFilePath))
+			{
+				File.Create(_appSettings.LastFilePath).Close();
+			}
+			_phoneRepository = _serviceProvider.GetRequiredService<PhoneRepository>();
 
-			DataTable.ItemsSource = _serviceProvider.GetRequiredService<PhoneRepository>().GetAll();
+			DataTable.ItemsSource = _phoneRepository.GetAll();
+
+
 		}
 
 		private void ChangeButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new NotImplementedException();
+			int columnIndex = DataTable.SelectedCells[0].Column.DisplayIndex;
+			int rowIndex = DataTable.Items.IndexOf(DataTable.SelectedCells[0].Item);
+
+			
 		}
 
 		private void DeleteButton_OnClick(object sender, RoutedEventArgs e)
@@ -72,12 +92,40 @@ namespace Lab_1
 
 		private void AddButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new NotImplementedException();
+			_serviceProvider.GetRequiredService<PhoneEntry>().Show();
 		}
 
 		private void MenuItem_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new NotImplementedException();
+			/*
+			 * 1. При нажатии на кнопку открывается окно выбора директории
+			 * 2. Если нажимаем Cancel, то выходим из функции
+			 * 3. Если выбрали файл, то
+			 *		3.1 обновляем appsettings,
+			 *		3.2 Меняем директорию в phoneRepos
+			 *		3.3 Обновляем таблицу
+			 */
+
+			var fileDialog = new OpenFileDialog();
+			var result = fileDialog.ShowDialog();
+			if (result == true)
+			{
+				var text = fileDialog.FileName;
+				_appSettings.LastFilePath = text;
+				_appSettings.UpdateConfigurationFile();
+			}
 		}
+
+		public void UpdateFunction()
+		{
+			DataTable.ItemsSource = _phoneRepository.GetAll();
+		}
+
+		private void RefreshButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			UpdateFunction();
+		}
+
+
 	}
 }
